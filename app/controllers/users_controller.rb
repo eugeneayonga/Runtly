48ac039207470f1f31213fa809c7be5cf33bfa79
Eugene <eugeneayonga@gmail.com>
2022-10-11 09:54:12 +0300

    Built show user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
         users = User.all
         render json: users, status: :ok
     end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user 
+            render json: user, status: :ok 
+        else
+            render json: {error: ["Not authorized"]}, status: :unauthorized 
+        end
+    end
 end
