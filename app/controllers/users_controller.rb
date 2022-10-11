810e969328bcbd5570213a7c239d26917019976b
Eugene <eugeneayonga@gmail.com>
2022-10-11 09:56:09 +0300

    Built create user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
         render json: users, status: :ok
     end
 
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
     def show
         user = User.find_by(id: session[:user_id])
         if user 
