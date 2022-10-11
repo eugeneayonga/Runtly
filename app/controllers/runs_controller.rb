492c821521b6bdab439ccd53e539fc4c05fa3325
Eugene <eugeneayonga@gmail.com>
2022-10-11 09:27:34 +0300

    Built show runs method

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -2,13 +2,20 @@ class RunsController < ApplicationController
 
     def index
         if user
-            render json: user.runs.all
+            render json: user.runs.all, status: :ok
         else
             render json: {error: ["Unauthorized Access"]}, status: :unauthorized
         end
     end
 
     def show
+        run = user.runs.find_by(id: params[:id])
+
+        if run
+            render json: run, status: :ok
+        else
+            render json: {error: ["Unauthorized Access"]}, status: :unauthorized
+        end
     end
 
     def create
