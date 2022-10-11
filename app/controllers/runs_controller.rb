d4231b687446512ff135819d836ccb44339359d9
Eugene <eugeneayonga@gmail.com>
2022-10-11 10:34:20 +0300

    Built user private method

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -14,7 +14,7 @@ class RunsController < ApplicationController
         if run
             render json: run, status: :ok
         else
-            render json: {error: "NOt found"}, status: :unauthorized
+            render json: {error: "Not found"}, status: :unauthorized
         end
     end
 
@@ -50,7 +50,6 @@ class RunsController < ApplicationController
     def fastest_mile
         run = user.runs.minimum(:fastest_split)
         if run 
-            
             render json: run, status: :ok 
         else 
             render json: {error: ["Enter some run data to find your fastest mile so far."]}, status: :not_found 
@@ -69,6 +68,10 @@ class RunsController < ApplicationController
     end
 
     private
+
+    def user
+        User.find_by(id: session[:user_id])
+    end
 
     def run_params 
         params.require(:run).permit(:date, :distance, :total_time, :calories, :elevation, :average_heartrate, :average_pace, :fastest_split, :favorite, :user_id)
